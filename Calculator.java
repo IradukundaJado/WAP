9e07b33cea7f7afc2a7e2c5c712c88e55206f513
Jean de Dieu Iradukunda <jadoiradukunda@gmail.com>
2019-05-03 16:24:00 -0500

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -18,22 +18,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class Calculator extends HttpServlet {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Calculator</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Calculator at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+    
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
